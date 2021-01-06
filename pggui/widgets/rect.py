b19dd0f7bd0573ee427838c5100613f3f4437957
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2021-01-07 00:43:47 +0100

    Add width in Rect

--- pggui/widgets/rect.py
+++ pggui/widgets/rect.py
@@ -5,7 +5,7 @@ from pggui.utils import Color, Vec2
 
 
 class Rect(Widget):
-    def __init__(self, position, size=Vec2.zero(), color=Color.from_name("BLACK")):
+    def __init__(self, position, size=Vec2.zero(), color=Color.from_name("BLACK"), width=0):
         """
             Create Rect Widget
 
@@ -17,10 +17,13 @@ class Rect(Widget):
                 Size of Rect
             color: Color
                 Color of Rect
+            width: int
+                Width of Rect (0 fill Rect)
         """
         super().__init__(position)
         self.size = size
         self.color = color
+        self.width = width
     
     def display(self, screen):
         """
@@ -29,4 +32,4 @@ class Rect(Widget):
             .. warning:: Don't use this function manually.
         """
         if self.displayed:
-            pygame.draw.rect(screen, self.color.get_rgba(), pygame.Rect(*self.position.coords(), *self.size.coords()))
+            pygame.draw.rect(screen, self.color.get_rgba(), pygame.Rect(*self.position.coords(), *self.size.coords()), self.width)
